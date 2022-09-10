8b316f1fcb96addeed52ef890a0ecdba1191e13b
Varadraj Kulkarni <varadraj.vinod.kulkarni@gmail.com>
2022-09-10 11:50:06 +0530

    video 21: designing home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ImageProcessingService } from '../image-processing.service';
+import { Product } from '../_model/product.model';
+import { ProductService } from '../_services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  productDetails = [];
+
+  constructor(private productService: ProductService,
+    private imageProcessingService: ImageProcessingService) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  public getAllProducts() {
+    this.productService.getAllProducts()
+    .pipe(
+      map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
+    )
+    .subscribe(
+      (resp: Product[]) => {
+        console.log(resp);
+        this.productDetails = resp;
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
   }
 
 }
