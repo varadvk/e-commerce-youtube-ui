76aeca15a25ea5ca0ba1f8ed163d078790adec3a
Varad <varadraj.vinod.kulkarni@gmail.com>
2022-12-11 12:18:18 +0530

    Video 38 and 39 :Adding search bar on home page and show product details page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,8 +27,15 @@ export class HomeComponent implements OnInit {
     this.getAllProducts();
   }
 
-  public getAllProducts() {
-    this.productService.getAllProducts(this.pageNumber)
+  searchByKeyword(searchkeyword) {
+    console.log(searchkeyword);
+    this.pageNumber = 0;
+    this.productDetails = [];
+    this.getAllProducts(searchkeyword);
+  }
+
+  public getAllProducts(searchKey: string = "") {
+    this.productService.getAllProducts(this.pageNumber, searchKey)
     .pipe(
       map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
     )
--- src/app/show-product-details/show-product-details.component.ts
+++ src/app/show-product-details/show-product-details.component.ts
@@ -30,9 +30,16 @@ export class ShowProductDetailsComponent implements OnInit {
     this.getAllProducts();
   }
 
-  public getAllProducts() {
+  searchByKeyword(searchkeyword) {
+    console.log(searchkeyword);
+    this.pageNumber = 0;
+    this.productDetails = [];
+    this.getAllProducts(searchkeyword);
+  }
+
+  public getAllProducts(searchKeyword: string = "") {
     this.showTable = false;
-    this.productService.getAllProducts(this.pageNumber)
+    this.productService.getAllProducts(this.pageNumber, searchKeyword)
     .pipe(
       map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
     )
